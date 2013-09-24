e75fcec1877142b7abbb67e6e9baa0c52cac8d55
benmcevoy <ben.mcevoy@gmail.com>
2013-09-24 17:45:32 +1000

    Added Phone test project

---
+++ Radio7.Phone.Rss.TestApp/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Radio7.Portable.Rss;
+
+namespace Radio7.Phone.Rss.TestApp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var url = Url.Text;
+
+            var feed = new Feed(new Uri(url, UriKind.Absolute));
+
+            feed.FeedLoaded += (o, args) =>
+            {
+                var firstOrDefault = feed.Items.FirstOrDefault();
+                WithDispatcher(() =>
+                {
+                    if (firstOrDefault != null) Results.Text = firstOrDefault.Title;
+                });
+            };
+
+            feed.GetItemsFromWeb();
+        }
+
+        private static void WithDispatcher(Action action)
+        {
+            Deployment.Current.Dispatcher.BeginInvoke(action);
+        }
+    }
+}
